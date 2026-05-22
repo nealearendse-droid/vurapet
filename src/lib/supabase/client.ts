a32e301b67d9be4347038740b36c6dfdd019662e
nealearendse-droid <neale.arends@gmail.com>
2026-05-22 23:42:01 +0200

    update supabase keys

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,6 +2,6 @@ import { createBrowserClient } from '@supabase/ssr';
 
 export const createSupabaseBrowserClient = () =>
   createBrowserClient(
-    'https://apdpeyvuhdawuftsxtku.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwZHBleXZ1aGRhd3VmdHN4dGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzMwOTEsImV4cCI6MjA5MTY0OTA5MX0.zgIqLG-UMvnhv_oSzd1x4B0NF89D32v1nNyd1hNepsE'
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
