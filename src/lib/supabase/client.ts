b463fd00496b37e6595f98a7c1f4c398ceca8520
nealearendse-droid <neale.arends@gmail.com>
2026-05-22 17:34:20 +0200

    fix supabase anon key

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,5 +3,5 @@ import { createBrowserClient } from '@supabase/ssr';
 export const createSupabaseBrowserClient = () =>
   createBrowserClient(
     'https://apdpeyvuhdawuftsxtku.supabase.co',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwZHBleXZ1aGRhd3VmdHN4dGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzMwOTEsImV4cCI6MjA5MTY0OTA5MX0.zgIqLG-UMvnhv_oSzd1x4B0NF89D32v1nNyd1hNepsE'
   );
